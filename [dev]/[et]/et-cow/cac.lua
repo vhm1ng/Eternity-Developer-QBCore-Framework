5bc717dd5c302f89b88d10d8cedfa149d4bc7cbe
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-05-17 14:24:55 +0700

    Create cac.lua

---
+++ [dev]/[et]/et-cow/cac.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'et-Cow'
+version '1.0.0'
+
+shared_scripts {
+	-- '@PolyZone/client.lua',
+	-- '@PolyZone/BoxZone.lua',
+	-- '@PolyZone/EntityZone.lua',
+	-- '@PolyZone/CircleZone.lua',
+	-- '@PolyZone/ComboZone.lua',
+    -- 'config.lua',
+}
+
+client_scripts {
+	-- 'config.lua',
+	-- 'client/*.lua',
+	-- '@PolyZone/client.lua',
+	-- '@PolyZone/BoxZone.lua',
+	-- '@PolyZone/EntityZone.lua',
+	-- '@PolyZone/CircleZone.lua',
+	-- '@PolyZone/ComboZone.lua',
+}
+
+
