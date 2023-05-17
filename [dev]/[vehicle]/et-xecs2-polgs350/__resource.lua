39f97d1c62b4c5f6cc5f582e1225bcb4ad6002f9
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-05-17 14:41:56 +0700

    Update __resource.lua

--- [dev]/[vehicle]/et-xecs2-polgs350/__resource.lua
+++ [dev]/[vehicle]/et-xecs2-polgs350/__resource.lua
@@ -4,14 +4,14 @@ description "Car Add-on"
 
 files {
     'data/**/handling.meta',
-    'data/**/vehiclelayouts.meta',
+    -- 'data/**/vehiclelayouts.meta',
     'data/**/vehicles.meta',
     'data/**/carcols.meta',
     'data/**/carvariations.meta',
 }
 
 data_file 'HANDLING_FILE' 'data/**/handling.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+-- data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
 data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
 data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
