93712b5decd176c7c455ecf0a51313fa54e819f3
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-05-24 15:37:08 +0700

    job cow

---
+++ [dev]/[addon]/jobcow/config.lua
@@ -0,0 +1,26 @@
+Cow = {}
+Cow.PlantsField = vector3(2432.98, 4765.31, 34.34)
+
+Cow.PedLocation = vector3(2270.87, 5153.08, 55.85)
+Cow.PedCoordsHeading = 237.86
+Cow.PedHash = `ig_old_man1a`
+Cow.Boi1 = vector3(1348.99, 4179.62, 30.19)
+
+
+Cow.PackingMilk = {
+    --Public Garage:
+	['packingmilk'] = {
+		zones = {
+            vector2(2502.6721191406, 4803.7915039062),
+            vector2(2508.2387695312, 4801.1059570312),
+            vector2(2505.8771972656, 4795.7314453125),
+            vector2(2499.91015625, 4798.802734375)
+        },
+        minZ = 34.434703826904,
+        maxZ = 34.617511749268
+    },
+}
+
+Cow.SecondaryColor = {r = 33, g = 244, b = 218, a = 255} -- Use RGB color picker
+Cow.nuongthit = {coords = vector3(454.27, -841.57, 27.62), name = 'blip_weedprocessing', color = 0, sprite = 6, radius = 10.0}
+--X: 454.92874 Y: -841.5122 Z: 27.647983 H: 73.541542
