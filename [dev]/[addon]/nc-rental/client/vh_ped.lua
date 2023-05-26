b9468b317688753781eb7567140d1b61d779eb7b
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-05-26 17:07:14 +0700

    Create vh_ped.lua

---
+++ [dev]/[addon]/nc-rental/client/vh_ped.lua
@@ -0,0 +1,43 @@
+local QBCore = exports['et-core']:GetCoreObject()
+
+CreateThread(function()
+    -- Blip
+    local blip = AddBlipForCoord(Config.CreatePed.x, Config.CreatePed.y, Config.CreatePed.z)
+    SetBlipSprite(blip, 326)
+    SetBlipDisplay(blip, 4)
+    SetBlipScale(blip, 0.7)
+    SetBlipAsShortRange(blip, true)
+    SetBlipColour(blip, 6)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentSubstringPlayerName("Thuê Xe")
+    EndTextCommandSetBlipName(blip)
+
+    -- Load ped model
+    local pedModel = `s_m_m_migrant_01`
+    RequestModel(pedModel)
+    while not HasModelLoaded(pedModel) do Wait(0) end
+
+    -- Create Ped
+    ped = CreatePed(0, pedModel, Config.CreatePed.x, Config.CreatePed.y, Config.CreatePed.z, Config.CreatePed.w, false, false)
+    FreezeEntityPosition(ped, true)
+    SetEntityInvincible(ped, true)
+    SetBlockingOfNonTemporaryEvents(ped, true)
+    TaskStartScenarioInPlace( ped, "WORLD_HUMAN_CLIPBOARD", 0, true)
+
+    -- Add et-target interaction
+    exports['et-target']:AddTargetEntity(ped, {
+        options = {
+            {
+                event = "et-rental:vehiclelist",
+                icon = "fas fa-circle",
+                label = "Rent vehicle",
+            },
+            {
+                event = "et-rental:returnvehicle",
+                icon = "fas fa-circle",
+                label = "Return Vehicle (Receive Back 50% of original price)",
+            },
+        },
+        distance = 3.5,
+    })
+end)
