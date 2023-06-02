4b55e1b41d7eed3dd4ae2928972dbfa8fa7d43aa
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-02 07:35:17 +0700

    Create CuaGiamTu2.lua

---
+++ [dev]/[et]/et-doorlock/configs/CuaGiamTu2.lua
@@ -0,0 +1,14 @@
+
+
+-- -1426024720 created by VHMING
+Config.DoorList['CuaGiamTu2--1426024720'] = {
+    doorRate = 1.0,
+    distance = 2,
+    fixText = false,
+    locked = true,
+    objYaw = 260.0202331543,
+    objCoords = vec3(586.367065, -9.138889, 75.616272),
+    doorType = 'sliding',
+    authorizedJobs = { ['police'] = 0 },
+    objName = -1426024720,
+}
