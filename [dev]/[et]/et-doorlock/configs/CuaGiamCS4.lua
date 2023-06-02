c96009a15c4e6cb20ee02cfb92ea2422439be7c4
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-02 07:34:49 +0700

    Create CuaGiamCS4.lua

---
+++ [dev]/[et]/et-doorlock/configs/CuaGiamCS4.lua
@@ -0,0 +1,14 @@
+
+
+-- -1426024720 created by VHMING
+Config.DoorList['CuaGiamCS4--1426024720'] = {
+    doorRate = 1.0,
+    distance = 2,
+    doors = {
+        {objName = -1320876379, objYaw = 260.0202331543, objCoords = vec3(577.344666, -23.981522, 76.778816)},
+        {objName = -1320876379, objYaw = 80.047630310059, objCoords = vec3(577.795593, -21.419201, 76.778816)}
+    },
+    doorType = 'double',
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+}
