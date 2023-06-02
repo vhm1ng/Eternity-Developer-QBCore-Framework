d9e7adde118b3dddd9949b62872dc9caac0d96a3
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-02 10:45:22 +0700

    Create Phong Admin BS.lua

---
+++ [dev]/[et]/et-doorlock/configs/Phong Admin BS.lua
@@ -0,0 +1,14 @@
+
+
+-- 854291622 created by VHMING
+Config.DoorList['Phong Admin BS-854291622'] = {
+    objName = 854291622,
+    distance = 2,
+    objCoords = vec3(339.004974, -586.703369, 43.433910),
+    fixText = false,
+    authorizedJobs = { ['ambulance'] = 0 },
+    doorRate = 1.0,
+    objYaw = 340.00003051758,
+    locked = true,
+    doorType = 'door',
+}
