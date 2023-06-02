629e6aa392a091443fc761745bb07f9f787c9aac
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-02 07:34:16 +0700

    Create CuaChinhCS.lua

---
+++ [dev]/[et]/et-doorlock/configs/CuaChinhCS.lua
@@ -0,0 +1,14 @@
+
+
+-- -1215222675 created by VHMING
+Config.DoorList['CuaChinhCS--1215222675'] = {
+    doorRate = 1.0,
+    distance = 2,
+    doors = {
+        {objName = -1215222675, objYaw = 70.056037902832, objCoords = vec3(637.183167, 0.876229, 82.935295)},
+        {objName = -1215222675, objYaw = 249.95581054688, objCoords = vec3(638.073059, 3.319880, 82.935295)}
+    },
+    doorType = 'double',
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+}
