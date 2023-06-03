1d78770f54f26c15716567a31559b15aaf6473b1
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-03 08:47:58 +0700

    add teddynguyen car

---
+++ [dev]/[vehicle]/teddynguyen-razerhycade34/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'bodacious'
+game 'gta5'
+ 
+-- fxmanifest.lua by VHMing
+
+description 'RazerCade34 của pé pò cute'
+version '1.0.0'
+author 'VHMing'
+
+
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+    'vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
