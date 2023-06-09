b2ad29221412441634063279978222f14c77360f
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-09 11:06:10 +0700

    ĐỊT MẸ SOURCE LỒN

--- [dev]/[addon]/vhming-guithongbao/server/main.lua
+++
@@ -1,9 +0,0 @@
-local QBCore = exports['et-core']:GetCoreObject()
-QBCore.Commands.Add('guithongbao', 'Gửi thông báo tới tất cả mọi người', { { name = 'Nội dung', help = 'Nhập nội dung muốn gửi tới' } }
-    ,
-    false,
-    function(source, args)
-        if #args < 1 then TriggerClientEvent('QBCore:Notify', source, Lang:t('error.missing_args2'), 'error') return end
-        local msg = table.concat(args, ' '):gsub('[~<].-[>~]', '')
-        TriggerClientEvent('guiThongBao', -1, msg)
-    end, 'admin')
