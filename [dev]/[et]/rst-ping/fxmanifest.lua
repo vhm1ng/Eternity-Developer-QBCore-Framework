94ff15c6072674e32656fa30e46522a4a72a2321
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-15 17:16:41 +0700

    _resource -> fxmanifest

---
+++ [dev]/[et]/rst-ping/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game {'gta5'}
+lua54 'yes'
+
+ui_page 'html/ui.html'
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+files {
+    'html/ui.html',
+    'html/*.css',
+    'html/*.js',
+    'html/img/*.png',
+}
+
+server_scripts { '@mysql-async/lib/MySQL.lua' }
+server_scripts { '@mysql-async/lib/MySQL.lua' }
