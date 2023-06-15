1cb0cb43292bbba3f187e188275b16fcc7542518
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-15 17:23:57 +0700

    Mày sảng rồi thanh ơiii

--- [defaultmaps]/[map-huynhdat]/HUYNHDAT-707-forest_mansion/__resource.lua
+++ [defaultmaps]/[map-huynhdat]/HUYNHDAT-707-forest_mansion/__resource.lua
@@ -1,15 +1,2 @@
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
- 
-files {
-    'vehicles.meta',
-    'carvariations.meta',
-    'carcols.meta',
-    'handling.meta',
-    'vehiclelayouts.meta',
-}
-
-data_file 'HANDLING_FILE' 'handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
-data_file 'CARCOLS_FILE' 'carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+this_is_a_map 'yes'
