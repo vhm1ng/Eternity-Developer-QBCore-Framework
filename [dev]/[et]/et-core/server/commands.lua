2477587342c5219136b0e5cde047fe3bcc002cdc
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-15 17:17:12 +0700

    Comment - OOC CHAT

--- [dev]/[et]/et-core/server/commands.lua
+++ [dev]/[et]/et-core/server/commands.lua
@@ -245,36 +245,36 @@ end, 'admin')
 
 -- Out of Character Chat
 
-QBCore.Commands.Add('ooc', Lang:t("command.ooc.help"), {}, false, function(source, args)
-    local message = table.concat(args, ' ')
-    local Players = QBCore.Functions.GetPlayers()
-    local Player = QBCore.Functions.GetPlayer(source)
-    local playerCoords = GetEntityCoords(GetPlayerPed(source))
-    for _, v in pairs(Players) do
-        if v == source then
-            TriggerClientEvent('chat:addMessage', v, {
-                color = { 0, 0, 255},
-                multiline = true,
-                args = {'OOC | '.. GetPlayerName(source), message}
-            })
-        elseif #(playerCoords - GetEntityCoords(GetPlayerPed(v))) < 20.0 then
-            TriggerClientEvent('chat:addMessage', v, {
-                color = { 0, 0, 255},
-                multiline = true,
-                args = {'OOC | '.. GetPlayerName(source), message}
-            })
-        elseif QBCore.Functions.HasPermission(v, 'admin') then
-            if QBCore.Functions.IsOptin(v) then
-                TriggerClientEvent('chat:addMessage', v, {
-                    color = { 0, 0, 255},
-                    multiline = true,
-                    args = {'Proxmity OOC | '.. GetPlayerName(source), message}
-                })
-                TriggerEvent('et-log:server:CreateLog', 'ooc', 'OOC', 'white', '**' .. GetPlayerName(source) .. '** (CitizenID: ' .. Player.PlayerData.citizenid .. ' | ID: ' .. source .. ') **Message:** ' .. message, false)
-            end
-        end
-    end
-end, 'user')
+-- QBCore.Commands.Add('ooc', Lang:t("command.ooc.help"), {}, false, function(source, args)
+--     local message = table.concat(args, ' ')
+--     local Players = QBCore.Functions.GetPlayers()
+--     local Player = QBCore.Functions.GetPlayer(source)
+--     local playerCoords = GetEntityCoords(GetPlayerPed(source))
+--     for _, v in pairs(Players) do
+--         if v == source then
+--             TriggerClientEvent('chat:addMessage', v, {
+--                 color = { 0, 0, 255},
+--                 multiline = true,
+--                 args = {'OOC | '.. GetPlayerName(source), message}
+--             })
+--         elseif #(playerCoords - GetEntityCoords(GetPlayerPed(v))) < 20.0 then
+--             TriggerClientEvent('chat:addMessage', v, {
+--                 color = { 0, 0, 255},
+--                 multiline = true,
+--                 args = {'OOC | '.. GetPlayerName(source), message}
+--             })
+--         elseif QBCore.Functions.HasPermission(v, 'admin') then
+--             if QBCore.Functions.IsOptin(v) then
+--                 TriggerClientEvent('chat:addMessage', v, {
+--                     color = { 0, 0, 255},
+--                     multiline = true,
+--                     args = {'Proxmity OOC | '.. GetPlayerName(source), message}
+--                 })
+--                 TriggerEvent('et-log:server:CreateLog', 'ooc', 'OOC', 'white', '**' .. GetPlayerName(source) .. '** (CitizenID: ' .. Player.PlayerData.citizenid .. ' | ID: ' .. source .. ') **Message:** ' .. message, false)
+--             end
+--         end
+--     end
+-- end, 'user')
 
 -- Me command
 
