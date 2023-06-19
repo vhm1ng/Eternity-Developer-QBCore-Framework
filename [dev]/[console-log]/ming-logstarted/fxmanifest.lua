b02f7cded3c319ba6a511b94f40542b30f4d8fd9
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-19 12:58:25 +0700

    Add Log Started

---
+++ [dev]/[console-log]/ming-logstarted/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+ 
+-- fxmanifest.lua by VHMing
+
+description 'Log Started | QBCore Print Log'
+version '1.0.0'
+author 'VHMing'
+
+client_script {
+    'client/*.lua'
+}
+
+server_script {
+    'server/*.lua'
+}
