0cab188742707ef028e5fd0958cbd5bcf463b1c4
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-21 13:08:53 +0700

    Update config.lua

--- [dev]/[et]/et-multicharacter/config.lua
+++ [dev]/[et]/et-multicharacter/config.lua
@@ -1,9 +1,9 @@
 Config = {}
-Config.Interior = vector3(-264.1443, -951.5657, 75.8179) -- Interior to load where characters are previewed
+Config.Interior = vector3(-1082.04, -2724.94, 0.81) -- Interior to load where characters are previewed
 Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
-Config.PedCoords = vector4(-256.3065, -941.7942, 75.8287, 156.7334) -- Create preview ped at these coordinates
-Config.HiddenCoords = vector4(-261.8508, -949.5483, 75.8287, 142.5654) -- Hides your actual ped while you are in selection
-Config.CamCoords = vector4(-258.5254, -943.3583, 76.1148, 309.1309) -- Camera coordinates for character preview screen
+Config.PedCoords = vector4(-1082.72, -2725.36, 0.81, 292.23) -- Create preview ped at these coordinates vector4(-1082.7, -2725.37, 0.81, 294.08)
+Config.HiddenCoords = vector4(-1082.72, -2725.36, 0.81, 292.23) -- Hides your actual ped while you are in selection
+Config.CamCoords = vector4(-1079.22, -2722.92, 0.81, 117.39) -- Camera coordinates for character preview screen
 Config.EnableDeleteButton = false -- Define if the player can delete the character or not
 
 Config.DefaultNumberOfCharacters = 1 -- Define maximum amount of default characters, Max 4 //ST4LTH
