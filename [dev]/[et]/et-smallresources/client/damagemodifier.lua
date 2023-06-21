a19ae6fc040da4481f4026f3a0336c4d7e26f17b
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-21 17:47:55 +0700

    Chỉnh damage tay

--- [dev]/[et]/et-smallresources/client/damagemodifier.lua
+++ [dev]/[et]/et-smallresources/client/damagemodifier.lua
@@ -1,3 +1,3 @@
 local QBCore = exports['et-core']:GetCoreObject()
 -- _SET_WEAPON_DAMAGE_MODIFIER
-SetWeaponDamageModifier(`WEAPON_CARBINERIFLE`, 0.8)
+SetWeaponDamageModifier(`weapon_unarmed`, 0.3)
