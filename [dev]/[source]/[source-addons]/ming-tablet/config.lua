e20fac5b7eac6bf4cd33021568ca5518dad08839
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-26 05:37:18 +0700

    add table luật

---
+++ [dev]/[source]/[source-addons]/ming-tablet/config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.Command = 'help'
+Config.CommandDescription = 'Mở máy tính bảng thông tin'
+
+Config.Keybind = true
+Config.OpenKey = 'F2'
+
+Config.AnimDict = 'amb@world_human_seat_wall_tablet@female@base'
+Config.Prop = 'prop_cs_tablet'
+Config.Bone = 28422
