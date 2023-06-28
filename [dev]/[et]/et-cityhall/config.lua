b1c7a2f4d0d9ba265c6d5663511d379a41e1d64d
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-28 09:08:02 +0700

    Update config.lua

--- [dev]/[et]/et-cityhall/config.lua
+++ [dev]/[et]/et-cityhall/config.lua
@@ -25,7 +25,7 @@ Config.Cityhalls = {
 Config.DrivingSchools = {
     { -- Driving School 1
         coords = vec3(240.3, -1379.89, 33.74),
-        showBlip = true,
+        showBlip = false,
         blipData = {
             sprite = 225,
             display = 4,
@@ -54,15 +54,4 @@ Config.Peds = {
             debugPoly = false
         }
     },
-    -- Driving School Ped
-    {
-        model = 'a_m_m_eastsa_02',
-        coords = vec4(240.91, -1379.2, 32.74, 138.96),
-        scenario = 'WORLD_HUMAN_STAND_MOBILE',
-        drivingschool = true,
-        zoneOptions = { -- Used for when UseTarget is false
-            length = 3.0,
-            width = 3.0
-        }
-    }
 }
