71c71d1be54d605a2bacac68e65d64eb06775187
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-28 07:28:02 +0700

    xóa 1 rst-ping + sửa lại et-phone

--- [dev]/[et]/et-phone/fxmanifest.lua
+++ [dev]/[et]/et-phone/fxmanifest.lua
@@ -1,8 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'et-Phone'
-version '1.2.0'
+author 'FjamZoo#0001 & MannyOnBrazzers#6826'
+description 'A No Pixel inspired edit of QBCore\'s Phone. Released By RenewedScripts'
+version 'Release'
 
 ui_page 'html/index.html'
 
@@ -13,13 +14,12 @@ shared_scripts {
 }
 
 client_scripts {
-    'client/main.lua',
-    'client/animation.lua'
+    'client/*.lua',
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server/main.lua'
+    'server/*.lua',
 }
 
 files {
@@ -27,12 +27,10 @@ files {
     'html/js/*.js',
     'html/img/*.png',
     'html/css/*.css',
-    -- 'html/fonts/*.ttf',
-    -- 'html/fonts/*.otf',
-    -- 'html/fonts/*.woff',
     'html/img/backgrounds/*.png',
     'html/img/apps/*.png',
 }
 
-
 lua54 'yes'
+
+dependency 'et-target'
