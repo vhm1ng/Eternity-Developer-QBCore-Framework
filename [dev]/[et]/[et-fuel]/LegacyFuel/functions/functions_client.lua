c7e7c2d3f9a71a00c2a3f9e32f01a88629c46d5c
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-30 07:22:03 +0700

    change blip scale

--- [dev]/[et]/[et-fuel]/LegacyFuel/functions/functions_client.lua
+++ [dev]/[et]/[et-fuel]/LegacyFuel/functions/functions_client.lua
@@ -29,7 +29,7 @@ function CreateBlip(coords)
 	local blip = AddBlipForCoord(coords)
 
 	SetBlipSprite(blip, 361)
-	SetBlipScale(blip, 0.9)
+	SetBlipScale(blip, 0.7)
 	SetBlipColour(blip, 4)
 	SetBlipDisplay(blip, 4)
 	SetBlipAsShortRange(blip, true)
