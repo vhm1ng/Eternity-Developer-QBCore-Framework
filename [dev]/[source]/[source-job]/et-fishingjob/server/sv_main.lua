b3bb5a30c8c78c2d0b90cc1c3d089017ab38702d
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-06-30 07:51:20 +0700

    Sửa tỉ lệ ra cá

--- [dev]/[source]/[source-job]/et-fishingjob/server/sv_main.lua
+++ [dev]/[source]/[source-job]/et-fishingjob/server/sv_main.lua
@@ -18,27 +18,25 @@ RegisterNetEvent('et-fishing:server:ReceiveFish', function()
     local random = math.random(100) -- Random number from 1 to 100
     local item
 
-    if random >= 1 and random <= 30 then -- 30%
+    if random >= 1 and random <= 26 then -- 26%
         item = 'fish'
-    elseif random >= 30 and random <= 37 then -- 7%
+    elseif random >= 26 and random <= 48 then -- 22%
         item = 'fish2'
-    elseif random >= 37 and random <= 44 then -- 7%
-        item = 'catfish'
-    elseif random >= 44 and random <= 51 then -- 7%
+    elseif random >= 48 and random <= 59 then -- 11%
         item = 'goldfish'
-    elseif random >= 51 and random <= 58 then -- 7%
+    elseif random >= 59 and random <= 70 then -- 11%
         item = 'largemouthbass'
-    elseif random >= 58 and random <= 65 then -- 7%
+    elseif random >= 70 and random <= 78 then -- 8%
         item = 'redfish'
-    elseif random >= 65 and random <= 72 then -- 7%
+    elseif random >= 78 and random <= 86 then -- 8%
         item = 'salmon'
-    elseif random >= 72 and random <= 79 then -- 7%
+    elseif random >= 86 and random <= 91 then -- 5%
         item = 'stingray'
-    elseif random >= 79 and random <= 86 then -- 7%
+    elseif random >= 91 and random <= 96 then -- 5%
         item = 'stripedbass'
-    elseif random >= 86 and random <= 93 then -- 7%
+    elseif random >= 96 and random <= 98 then -- 2%
         item = 'whale'
-    elseif random >= 93 and random <= 100 then -- 7%
+    elseif random >= 98 and random <= 100 then -- 2%
         item = 'whale2'
     end
 
