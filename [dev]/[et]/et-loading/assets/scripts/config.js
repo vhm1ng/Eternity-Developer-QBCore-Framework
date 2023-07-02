64ed9a2414db1bed06999bfe5131395f310d0a44
HM <vhm1ng@vnpxe.net>
2023-07-02 13:07:59 +0700

    Update config.js

--- [dev]/[et]/et-loading/assets/scripts/config.js
+++ [dev]/[et]/et-loading/assets/scripts/config.js
@@ -1,6 +1,6 @@
 Config = {}; // Don't touch
 
-Config.ServerIP = "113.175.188.99:30120";
+Config.ServerIP = "rp-eternity.ddns.net:30120";
 
 // Social media buttons on the left side
 Config.Socials = [
@@ -16,13 +16,14 @@ Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in 
 Config.Staff = [
     {name: "VHMing #1", description: "Admin & Developed", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/478160040683896832/81a090daec27adf5df1029c62b0f6028.png?size=2048"},
     {name: "ThanhNQ #2", description: "Admin & TTC", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/962331753882722314/9c7ac26e34163ebe128646c9fdcdbf51.png?size=2048"},
-    {name: "DRLord #3", description: "Admin & TTC", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/1098295677475373107/a_2cc06335ea55c90140235a4f0fa8d025.gif?size=2048"},
-    {name: "Huynh Dat #4", description: "Admin & TTC", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/798219312841621534/a9018a1036ba96ea91b1440c3bee4dbf.png?size=2048"},
-    {name: "Thanh Doki #5", description: "Staff", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/867090046142382110/c182e5513f3a10589d3fe3d38630ee2e.png?size=2048"},
-    {name: "Ai dậy ta #6", description: "Staff", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/842146148923998209/d3c6c833ec8b2b12881f5aedb4ea87c8.png?size=2048"},
-    {name: "Loki #7", description: "Staff", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/689166884687708167/f0bf08adc99edc2c4850b3db643b5a59.png?size=2048"},
-    {name: "Hades #8", description: "Staff", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/1104078584005529701/0cf2334b91b5a001f2ef83c30675b695.png?size=2048"},
-    {name: "Mynh Bu #9", description: "Developed", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/430940414069964800/edcf103b6cba5049994978f2c9090510.png?size=2048"},
+    {name: "SHL #3", description: "Developed", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/430940414069964800/edcf103b6cba5049994978f2c9090510.png?size=2048"},
+    {name: "DRLord #4", description: "Admin & TTC", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/1098295677475373107/a_2cc06335ea55c90140235a4f0fa8d025.gif?size=2048"},
+    {name: "Huynh Dat #5", description: "Admin & TTC", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/798219312841621534/a9018a1036ba96ea91b1440c3bee4dbf.png?size=2048"},
+    {name: "Thanh Doki #6", description: "Staff", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/867090046142382110/c182e5513f3a10589d3fe3d38630ee2e.png?size=2048"},
+    {name: "Ai dậy ta #7", description: "Staff", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/842146148923998209/d3c6c833ec8b2b12881f5aedb4ea87c8.png?size=2048"},
+    {name: "Loki #8", description: "Staff", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/689166884687708167/f0bf08adc99edc2c4850b3db643b5a59.png?size=2048"},
+    {name: "Hades #9", description: "Staff", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/1104078584005529701/0cf2334b91b5a001f2ef83c30675b695.png?size=2048"},
+    {name: "Mynh Bu #10", description: "Developed", color: "#ff0000", image: "https://cdn.discordapp.com/avatars/328490200638685187/232388c8465c94cbba8837917af24ffe.png?size=2048"},
 
 ];
 
