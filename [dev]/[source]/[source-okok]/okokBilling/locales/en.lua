aed76556e2d8e04ab9b43547d08a3a0b2cd0e64f
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-07-02 19:23:27 +0700

    Việt hóa bệnh viên

--- [dev]/[source]/[source-okok]/okokBilling/locales/en.lua
+++ [dev]/[source]/[source-okok]/okokBilling/locales/en.lua
@@ -2,78 +2,78 @@ Locales['en'] = {
 
 	-- Notifications
 	['created_success'] = {
-		title = 'Invoice',
-		text = 'The invoice was created successfully',
+		title = 'Hóa đơn',
+		text = 'Hóa đơn đã được tạo thành công',
 		time = 5000,
 		type = 'success'
 	},
 
 	['created_error'] = {
-		title = 'Invoice',
-		text = 'The invoice could not be created',
+		title = 'Hóa đơn',
+		text = 'Không thể tạo hóa đơn',
 		time = 5000,
 		type = 'error'
 	},
 
 	['received_invoice'] = {
-		title = 'Invoice',
-		text = 'You received an invoice',
+		title = 'Hóa đơn',
+		text = 'Bạn đã nhận được hóa đơn',
 		time = 5000,
 		type = 'success'
 	},
 
 	['paid_success'] = {
-		title = 'Invoice',
-		text = 'The invoice was paid successfully',
+		title = 'Hóa đơn',
+		text = 'Hóa đơn đã được thanh toán thành công',
 		time = 5000,
 		type = 'success'
 	},
 
 	['paid_error'] = {
-		title = 'Invoice',
-		text = 'The invoice could not be paid',
+		title = 'Hóa đơn',
+		text = 'Hóa đơn không thể thanh toán',
 		time = 5000,
 		type = 'error'
 	},
 
 	['near_error'] = {
-		title = 'Invoice',
-		text = 'There aren\'t near players in your area',
+		title = 'Hóa đơn',
+		text = 'Không có người chơi nào ở gần khu vực của bạn',
 		time = 5000,
 		type = 'error'
 	},
 
 	['send_as_soc_error'] = {
-		title = 'Invoice',
-		text = 'Your society can\'t send invoices',
+		title = 'Hóa đơn',
+		text = 'Ban hành của bạn không thể gửi hóa đơn',
 		time = 5000,
 		type = 'error'
 	},
 
 	['removed_invoice'] = {
-		title = 'Invoice',
-		text = 'Invoice has been removed successfully',
+		title = 'Hóa đơn',
+		text = 'Hóa đơn đã được xóa thành công',
 		time = 5000,
 		type = 'success'
 	},
 
 	['removed_invoice_error'] = {
-		title = 'Invoice',
-		text = 'Unable to delete invoice, please try again',
+		title = 'Hóa đơn',
+		text = 'Không thể xóa hóa đơn, vui lòng thử lại',
 		time = 5000,
 		type = 'error'
 	},
 	
 	['citizen_not_found'] = {
-		title = 'Invoice',
-		text = 'No citizen was found',
+		title = 'Hóa đơn',
+		text = 'Không tìm thấy công dân nào',
 		time = 5000,
 		type = 'error'
 	},
 
 	['okokRequests'] = {
-		title = 'Invoice',
-		text = 'Someone want\'s to send you an Invoice',
+		title = 'Hóa đơn',
+		text = 'Ai đó muốn gửi cho bạn một Hóa đơn',
 		time = 20000,
 	},
 }
