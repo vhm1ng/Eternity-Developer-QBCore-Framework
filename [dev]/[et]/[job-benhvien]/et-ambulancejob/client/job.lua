32a2fd2c7fa22c25ef075c982d24ba3879ecacf5
ImMrT0 <immrt0910@gmail.com>
2023-07-09 02:52:51 +0700

    Fix onduty cứu người

--- [dev]/[et]/[job-benhvien]/et-ambulancejob/client/job.lua
+++ [dev]/[et]/[job-benhvien]/et-ambulancejob/client/job.lua
@@ -212,33 +212,37 @@ end
 -- end)
 
 RegisterNetEvent('hospital:client:RevivePlayer', function()
-    local hasItem = QBCore.Functions.HasItem('firstaid')
-    if hasItem then
-        local player, distance = GetClosestPlayer()
-        if player ~= -1 and distance < 5.0 then
-            local playerId = GetPlayerServerId(player)
-            QBCore.Functions.Progressbar("hospital_revive", Lang:t('progress.revive'), 5000, false, true, {
-                disableMovement = false,
-                disableCarMovement = false,
-                disableMouse = false,
-                disableCombat = true,
-            }, {
-                animDict = healAnimDict,
-                anim = healAnim,
-                flags = 16,
-            }, {}, {}, function() -- Done
-                StopAnimTask(PlayerPedId(), healAnimDict, "exit", 1.0)
-                QBCore.Functions.Notify(Lang:t('success.revived'), 'success')
-                TriggerServerEvent("hospital:server:RevivePlayer", playerId)
-            end, function() -- Cancel
-                StopAnimTask(PlayerPedId(), healAnimDict, "exit", 1.0)
-                QBCore.Functions.Notify(Lang:t('error.canceled'), "error")
-            end)
+    if onDuty then
+        local hasItem = QBCore.Functions.HasItem('firstaid')
+        if hasItem then
+            local player, distance = GetClosestPlayer()
+            if player ~= -1 and distance < 5.0 then
+                local playerId = GetPlayerServerId(player)
+                QBCore.Functions.Progressbar("hospital_revive", Lang:t('progress.revive'), 5000, false, true, {
+                    disableMovement = false,
+                    disableCarMovement = false,
+                    disableMouse = false,
+                    disableCombat = true,
+                }, {
+                    animDict = healAnimDict,
+                    anim = healAnim,
+                    flags = 16,
+                }, {}, {}, function() -- Done
+                    StopAnimTask(PlayerPedId(), healAnimDict, "exit", 1.0)
+                    QBCore.Functions.Notify(Lang:t('success.revived'), 'success')
+                    TriggerServerEvent("hospital:server:RevivePlayer", playerId)
+                end, function() -- Cancel
+                    StopAnimTask(PlayerPedId(), healAnimDict, "exit", 1.0)
+                    QBCore.Functions.Notify(Lang:t('error.canceled'), "error")
+                end)
+            else
+                QBCore.Functions.Notify(Lang:t('error.no_player'), "error")
+            end
         else
-            QBCore.Functions.Notify(Lang:t('error.no_player'), "error")
+            QBCore.Functions.Notify(Lang:t('error.no_firstaid'), "error")
         end
     else
-        QBCore.Functions.Notify(Lang:t('error.no_firstaid'), "error")
+        QBCore.Functions.Notify("Bạn không có trong ca trực!", "error")
     end
 end)
 
