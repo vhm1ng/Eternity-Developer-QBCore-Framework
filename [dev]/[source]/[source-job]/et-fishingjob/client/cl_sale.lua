52313cf3febebc66678d4fdd2396b93f390ee158
ImMrT0 <immrt0910@gmail.com>
2023-07-09 02:49:52 +0700

    Tắt blips quanh Chợ Triều Khúc

--- [dev]/[source]/[source-job]/et-fishingjob/client/cl_sale.lua
+++ [dev]/[source]/[source-job]/et-fishingjob/client/cl_sale.lua
@@ -1,17 +1,6 @@
 local ped
 
 CreateThread(function()
-    -- Blip
-    local blip = AddBlipForCoord(Shared.SellLocation.x, Shared.SellLocation.y, Shared.SellLocation.z)
-    SetBlipSprite (blip, 628)
-    SetBlipDisplay(blip, 4)
-    SetBlipScale  (blip, 0.7)
-    SetBlipAsShortRange(blip, true)
-    SetBlipColour(blip, 3)
-    BeginTextCommandSetBlipName("STRING")
-    AddTextComponentSubstringPlayerName("Chợ bán cá")
-    EndTextCommandSetBlipName(blip)
-
     -- Load ped model
     local pedModel = `s_m_m_migrant_01`
     RequestModel(pedModel)
