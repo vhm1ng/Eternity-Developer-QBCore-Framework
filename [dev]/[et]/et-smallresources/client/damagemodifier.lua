1aaf68469cfe7049b7f7a6681bf1b9cd6814b0bc
ImMrT0 <immrt0910@gmail.com>
2023-07-08 12:47:41 +0700

    Tắt damage chí mạng

--- [dev]/[et]/et-smallresources/client/damagemodifier.lua
+++ [dev]/[et]/et-smallresources/client/damagemodifier.lua
@@ -1,4 +1,5 @@
 local QBCore = exports['et-core']:GetCoreObject()
 -- _SET_WEAPON_DAMAGE_MODIFIER
+SetPedSuffersCriticalHits(PlayerPedId(), false) -- BẬT / TẮT DAMAGE CHÍ MẠNG
 SetWeaponDamageModifier(`weapon_unarmed`, 0.3)
 SetWeaponDamageModifier(`weapon_battleaxe`, 0)
