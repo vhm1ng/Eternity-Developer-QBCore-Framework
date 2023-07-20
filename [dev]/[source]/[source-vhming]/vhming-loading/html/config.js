6a059a68e1fdef4235a97a39b1e7f0fe877e784b
VHMing sadboiz <132770113+dev-vhming@users.noreply.github.com>
2023-07-21 03:32:01 +0700

    Loading ... Ver2

---
+++ [dev]/[source]/[source-vhming]/vhming-loading/html/config.js
@@ -0,0 +1,61 @@
+const server = [{
+        logo: "logo", //FILE NAME png recommended
+        video: "https://www.youtube.com/embed/gYO1uk7vIcc"
+    }
+
+]
+
+const loadingsettings = [{
+        progressPercentage: 80
+    },
+
+]
+
+// TEAM NOTE reproducible
+const team = [{
+        name: "VHMing",
+        rank: "Developed // ETRP-HM",
+        image: "hihi",
+    },
+    {
+        name: "SHL",
+        rank: "Developed // ETRP-HM",
+        image: "shl",
+    },
+    {
+        name: "Huynh Dat",
+        rank: "Admin // ETRP-HM",
+        image: "hd",
+    },
+    {
+        name: "Tieu Nhi",
+        rank: "Admin // ETRP-HM",
+        image: "lo",
+    },
+    {
+        name: "ThanhNQ",
+        rank: "Admin // ETRP-HM",
+        image: "t",
+    },
+    {
+        name: "TeddyNguyen",
+        rank: "Support // ETRP-HM",
+        image: "adt",
+    },
+]
+
+// SOCIAL MEDIA
+const social = [{
+        discord: "Discord.gg/eternity-rp",
+        web: "Eternity.vnpxe.net",
+        instagram: "ET.Version - 2.0"
+    },
+
+]
+
+const sound = [{
+        musicname: "Elderbrook - Inner Light",
+        music: "nchub" //FILE NAME
+    },
+
+]
