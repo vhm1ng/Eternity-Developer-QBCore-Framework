37c1af733473a9e119beebc1b8936952cb6df5d2
VHMing <vhm1ng@vnpxe.net>
2023-07-21 02:12:43 +0700

    Update config.lua

--- [dev]/[et]/et-smallresources/config.lua
+++ [dev]/[et]/et-smallresources/config.lua
@@ -237,7 +237,7 @@ Config.BlacklistedVehs = {
     [`rrocket`] = true,
     [`ruiner2`] = true,
     [`deluxo`] = true,
-    -- [`policeb`] = true,
+    [`policeb`] = true,
     [`polmav`] = true,
 }
 
